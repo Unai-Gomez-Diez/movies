c78c5db41489790892cef909c4b7ac3e1ff7118e
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2024-01-22 13:02:22 +0100

    inicio 2

--- src/app/movie-plot/movie-plot.component.ts
+++ src/app/movie-plot/movie-plot.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Movie} from "../Shared/Movie";
 
 @Component({
   selector: 'app-movie-plot',
-  standalone: true,
-  imports: [],
   templateUrl: './movie-plot.component.html',
   styleUrl: './movie-plot.component.css'
 })
 export class MoviePlotComponent {
-
+  @Input() moviePlot:Movie | undefined
 }
