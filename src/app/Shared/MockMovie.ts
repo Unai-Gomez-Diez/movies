9c749bf2c134ce311180d1285f40adf63a13d6be
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2024-01-23 13:15:13 +0100

    funciona, debo mejorar un poco el diseño

--- src/app/Shared/MockMovie.ts
+++ src/app/Shared/MockMovie.ts
@@ -7,7 +7,7 @@ export const Movies:Movie[]=[
     Genre:"Action, Crime, Thriller",
     Actors:"Vin Diesel, Paul Walker, Jason Statham, Michelle Rodriguez",
     Plot:"Deckard Shaw seeks revenge against Dominic Toretto and his family for his comatose brother.",
-    Poster: "assets/Image/Furious7.jpg"
+    Poster: "assets/image/furious7.jpg"
   },
   {
     Title:"Batman",
@@ -15,7 +15,7 @@ export const Movies:Movie[]=[
     Genre:"Action, Adventure",
     Actors:"Michael Keaton, Jack Nicholson, Kim Basinger, Robert Wuhl",
     Plot:"The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker.",
-    Poster: "assets/Image/Batman.jpg"
+    Poster: "assets/image/batman.jpg"
   },
   {
     Title:"Skyfall",
@@ -23,7 +23,7 @@ export const Movies:Movie[]=[
     Genre:"Action, Adventure, Thriller",
     Actors:"Daniel Craig, Judi Dench, Javier Bardem, Ralph Fiennes",
     Plot:"Bond's loyalty to M is tested when her past comes back to haunt her. Whilst MI6 comes under attack, 007 must track down and destroy the threat, no matter how personal the cost.",
-    Poster: "assets/Image/Skyfall.jpg"
+    Poster: "assets/image/skyfall.jpg"
   },
   {
     Title:"Avatar",
@@ -31,7 +31,7 @@ export const Movies:Movie[]=[
     Genre:"Action, Adventure, Fantasy",
     Actors:"Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
     Plot:"A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
-    Poster: "assets/Image/Avatar.jpg"
+    Poster: "assets/image/avatar.jpg"
   },
   {
     Title:"Kill Bill: Vol. 1",
@@ -39,6 +39,6 @@ export const Movies:Movie[]=[
     Genre:"Action, Crime, Thriller",
     Actors:"Uma Thurman, Lucy Liu, Vivica A. Fox, Daryl Hannah",
     Plot:"The Bride wakens from a four-year coma. The child she carried in her womb is gone. Now she must wreak vengeance on the team of assassins who betrayed her - a team she was once part of.",
-    Poster:"assets/Image/KillBill.jpg"
+    Poster:"assets/image/killbill.jpg"
   }
 ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MovieListComponent } from "./movie-list/movie-list.component";
 import {MoviePlotComponent} from "./movie-plot/movie-plot.component";
 import {MoviePosterComponent} from "./movie-poster/movie-poster.component";
+import {HeaderComponent} from "./header/header.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieListComponent,
     MoviePlotComponent,
-    MoviePosterComponent
+    MoviePosterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
